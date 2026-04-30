19b7d2f82ac70b7e1f5726dad60e4e19a977c1c7
Luchko Roman <romander09@gmail.com>
2026-04-30 16:25:33 +0300

    feat(): Configure webClient

---
+++ src/main/java/com/techstars/jobstechstars/controllers/JobController.java
@@ -0,0 +1,12 @@
+package com.techstars.jobstechstars.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/jobs")
+@RequiredArgsConstructor
+public class JobController {
+
+}
